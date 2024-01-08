fca4cd7976536162733c173edd89fcef4505e0db
Mustafizur Rahman <sojibvikings@gmail.com>
2024-01-08 14:42:55 +0600

    for live

--- src/configs/camp_api.js
+++ src/configs/camp_api.js
@@ -1,15 +1,15 @@
-export default {
-  protocol: 'http://',
-  base_url: '.localhost:8080',
-  loginApi: '/login/',
-  authMe: '/auth_me',
-  getCampUsers: '/get_camp_user'
-}
-
 // export default {
-//   protocol: 'https://',
-//   base_url: '.campcloudpro.com',
+//   protocol: 'http://',
+//   base_url: '.localhost:8080',
 //   loginApi: '/login/',
 //   authMe: '/auth_me',
 //   getCampUsers: '/get_camp_user'
 // }
+
+export default {
+  protocol: 'https://',
+  base_url: '.campcloudpro.com',
+  loginApi: '/login/',
+  authMe: '/auth_me',
+  getCampUsers: '/get_camp_user'
+}
